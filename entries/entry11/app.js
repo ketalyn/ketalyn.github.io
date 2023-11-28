8923b9cb0f965d5fa3247578af416733700a002b
ketalyn <leek105@newschool.edu>
2023-11-27 23:11:44 -0500

    for now

---
+++ entries/entry11/app.js
@@ -0,0 +1,236 @@
+let myQuestions = ['Which scent do you find the most soothing?',
+                    'What is a language you love to listen to even if you don’t speak or understand it?',
+                    'Oceans or mountains? Why?',
+                    'Describe the most captivating painting or artwork you’ve ever seen.',
+                    'Do you remember the first novel you ever read? If so, what was it?',
+                    'What is your favorite thing about your personality?',
+                    'If you could have a fictional superhero for a best friend, who would it be?',
+                    'What color would you choose to describe yourself?',
+                    'What is something you have accomplished as an adult that your younger self would be proud of?',
+                    'Is there a place in the world that you feel most represents who you are?',
+                    'Where is one place you’d love to travel to again?',
+'If your pet could talk, what would their voice sound like? What would they say about you?',
+'What tasks make you feel like your best self?',
+'Where do you feel most centered and happy?',
+'What’s your favorite comfort food?',
+'What do you wear when you’re feeling your very best, and why?',
+'What is the best meal you’ve ever had, and what’s the best meal you’ve ever cooked for yourself?',
+'What do you love most about your home?',
+'What’s a yearbook-style superlative you’d give to your high school self? What’s one for your current self?',
+'What gives you goosebumps?',
+'Do you know your astrology sign? How about your birth chart?',
+'Do you have any tattoos?',
+'Scuba diving or skydiving?',
+'What are you most grateful for in this season of life?',
+'Do you give back or volunteer with any organizations?',
+'What’s your love language?',
+What do you look for and need in your friendships?
+How do you feel that you best offer love and support to your friends?
+Tell me about your childhood best friend.
+When do you feel most authentically yourself?
+What’s one form of self-expression you’ve been too hesitant to explore?
+What’s one habit you want to get rid of and one habit you want to keep?
+If you were to perform a duet with a famous musician, who would it be and why?
+Do you have any recurring dreams? If so, what do you think they are trying to tell you?
+Who do you most admire, and how has that impacted the way you live your life?
+What’s one thing you’d tell yourself at my age? What’s one thing your younger self would tell you?
+What do you wish you’d known before having kids?
+What was the first big purchase you made as an adult?
+How did you know when you fell in love?
+Have you kept any memorabilia from your childhood?
+What do you miss most about being a child? A teenager? My age?
+What about the current world would be most surprising to your younger self?
+If you could travel back in time, which part of your life would you go back to?
+Who did you vote for in past elections? Why?
+What are some of your favorite memories about your parents?
+Tell me about you.
+Working on anything exciting/good lately?
+What’s your story?
+What personal passion project are you working on right now?
+What was the highlight of your day today?
+What was the highlight of your week?
+Have you eaten today?
+When was the last time you drank water?
+What was the high-point and low-point of your day so far?
+Has this been a busy time for you?
+What are you doing this weekend?
+What are your favorite restaurants around here?
+If you had to pick any character in a book, movie, or TV show who is most similar to you, who would you choose? Why? 
+When you were growing up, what was your dream job? Is any part of that still true?
+What’s your biggest fear?
+What’s your biggest regret? 
+Who is your role model?
+Is there a charitable cause you support?
+What job did you want to do when you were a kid?
+If you had to pick one–skydiving, bungee jumping, or scuba diving–which would you do?
+What’s the most important thing I should know about you?
+When you were a kid, what did you think your life would look like now?
+Which of your family members are you most like?
+Do you think there are aliens on other planets?
+If you were an animal, what animal would you be? Why
+Who would win in a fight, a robot or a dinosaur?
+What was your worst fashion disaster?
+What’s the most embarrassing thing you did at school / when you were a kid?
+If you had a reality TV show about your life, what would your theme song be?
+What’s your plan if there was a zombie apocalypse?
+What made you smile today?
+What's your favorite form of social media?
+What was the last good book you read?
+Do you listen to any podcasts? Which is your favorite?
+What do you think is the best show on Netflix right now?
+Have you been on any interesting trips lately?
+What do you think has been the best movie of the year so far?
+What song do you wish you could put on right now?
+Are you a cat person or a dog person?
+Do you think you're an introvert or an extrovert?
+What's your strangest hidden talent?
+What is something people are always surprised to learn about you?
+Where do you want to be in five years?
+What superpower do you wish you could have?
+Where would you go on vacation if you had no budget?
+If you could travel back in time, what decade would you choose to live in?
+What's the best thing you've ever bought off Amazon?
+What's the last concert you went to?
+What is one thing you can't live without?
+What's the strangest dream you've had recently?
+What is your favorite book of all time?
+How many countries have you been to?
+What's your favorite city you've visited?
+Would you rather travel via plane or boat?
+Would you rather be really hot or really cold?
+What are your thoughts on the British royal family?
+Do you like documentaries? Have you watched any good ones recently?
+Who is your favorite celebrity couple ever?
+Which celebrity couple do you wish would get back together?
+If you could host a talk show, who would you have on first?
+What's your favorite sport?
+What sport do you wish you were really good at?
+What's the best holiday?
+How did you spend your last birthday?
+Do you believe men and women can ever just be friends?
+Do you have any pets?
+What's one interesting thing about yourself no one really knows?
+If you could move anywhere, where would you go?
+What was the last movie you saw?
+What do you normally do for fun?
+What hobby do you wish you had more time for?
+What is your favorite holiday?
+Do you have any nicknames?
+What's your biggest pet peeve?
+What is your favorite guilty pleasure TV show?
+Tell me three fun facts about yourself.
+Have you ever been here before?
+Where did you grow up?
+Do you like to cook?
+What hobby do you wish you were good at?
+What song have you had stuck in your head this week?
+What are your long-term goals?
+What are the top three things on your bucket list?
+What has been your biggest accomplishment so far?
+What is one thing you wish you could do that you know you probably never will?
+What are you most afraid of?
+What's the biggest risk you've ever taken?
+Tell me about the most life-changing piece of advice you've ever heard.
+When are you the most happy?
+What always calms you down when you're really stressed out and upset?
+Who in your life has had the most influence on you?
+What's the hardest thing you've ever done?
+What scares you the most about the future?
+How have your priorities changed in the last 10 years?
+What's the nicest thing anyone has ever done for you?
+Describe your perfect weekend.
+What's the most controversial opinion you have?
+If you could change one thing about your personality, what would it be?
+What's the first thing you would do if you won the lottery?
+If you could invite one famous person to dinner, who would it be?
+Who do you miss the most from your past?
+If you could go back in time, what is one situation you would do differently?
+Who is the most important person in your life right now?
+What did you think you would be doing at this age when you were a kid?
+Do you think social media is destroying our communication?
+What piece of technology could you live without?
+What do you think the world will be like 20 years in the future?
+What is something you've failed at recently?
+What are your goals when it comes to work?
+If you could change one thing about this industry, what would it be?
+How many emails have you been avoiding answering this week?
+How do you keep yourself organized every day?
+What's the biggest challenge you've taken on this year?
+What's your favorite project you've worked on so far?
+Do you ever sing in the shower?
+What is the worst piece of advice you've ever gotten?
+What's the most embarrassing thing that's happened to you recently?
+Tell me about the worst pickup line you've ever gotten.
+What is your favorite celebrity scandal right now?
+What's the worst thing you've ever worn?
+If you could do anything illegal without getting in trouble, what would it be?
+What is the stupidest joke you've ever heard?
+When was the last time you laughed so hard you cried?
+What's the best prank you've ever played on someone?
+What do you think is the funniest movie ever?
+What always makes you laugh, even when you're upset?
+Who is your favorite comedian?
+What weird conspiracy theory do you believe?
+Is a hot dog a sandwich?
+Which celebrity would play you in a movie about your life?
+What's the weirdest thing you loved as a child?
+What is something that's really popular right now that will be ridiculous in five years?
+What's the worst trend you've ever taken part in?
+What's your go-to joke?
+What's your favorite color?
+What are you doing tomorrow?
+What food do you hate?
+What is one thing that always makes you laugh?
+How did you become close with your best friend?
+What's your favorite movie that you could watch over and over again?
+What has always been your dream job?
+If you could date one celebrity, who would it be?
+Are you a spender or a saver?
+What do you like most about your job?
+What do you think is your most annoying quirk?
+What's the weirdest text message you've ever gotten?
+Where's your favorite place in the world?
+What do you hate the most about dating?
+What's the best vacation you've ever been on?
+How close are you with your family?
+Do you prefer to stay in or go out?
+Have you ever lived anywhere else?
+What are you good at that you're really proud of?
+What do you think is the most annoying habit?
+Tell me about one thing on your bucket list.
+What do you dislike most about your job?
+What are you the most passionate about?
+Do you like calling or texting better?
+What are your honest thoughts about social media?
+What do you look for in a relationship?
+What's the worst thing you've ever eaten?
+What movie are you really excited to see?
+How do you spend your free time?
+Are you a morning person or a night owl?
+Where would you rather be right now?
+If you could only eat one food for the rest of your life, what would it be?
+What's your best tip for saving money?
+What is your favorite quote right now?
+How are your parents doing?
+What's your best cooking tip?
+Where is your favorite place to shop?
+Where is the coolest place you've been to for dinner lately?
+What's your go-to dish to bring to a dinner party?
+What did you do last weekend?
+What app do you use everyday?
+What's the weirdest thing in your fridge right now?
+What are your plans for next summer?
+What's your favorite thing you've bought recently?
+How can you tell when someone is lying?
+What's your most irrational fear?
+Have you ever really kept a New Year’s resolution?
+If you could change your name, what would you change it to?
+How would your life be different if social media didn't exist?
+What do you think my best personality trait is?
+What did you notice first about me?
+What's your favorite thing to do on your day off work?
+What was the first job you ever had?
+What's the best compliment you've ever gotten?
+
+
+];
